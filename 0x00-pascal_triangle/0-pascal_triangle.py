e84d68f685910c96956e21da282e00780900a4af
HindZiki <hind23051@gmail.com>
2024-10-03 19:38:46 +0000

    2

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,13 +2,12 @@ def pascal_triangle(n):
     if n <= 0:
         return []
 
-    triangle = [[1]]  # Première ligne du triangle
+    triangle = [[1]]  # Commence avec la première ligne
 
     for i in range(1, n):
         row = [1]  # Chaque ligne commence par 1
         for j in range(1, i):
-            # Chaque élément est la somme des deux éléments situés au-dessus
-            row.append(triangle[i-1][j-1] + triangle[i-1][j])
+            row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
         row.append(1)  # Chaque ligne se termine par 1
         triangle.append(row)
 
