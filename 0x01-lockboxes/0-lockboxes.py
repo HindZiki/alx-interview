64f7a37236f00d5ff90e3112c082a766e5b7316c
HindZiki <hind23051@gmail.com>
2024-10-13 22:17:50 +0000

    2

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,14 +1,16 @@
+#!/usr/bin/python3
+"""lockboxs""" 
 def canUnlockAll(boxes):
-    n = len(boxes)  # Nombre total de boîtes
-    opened = [False] * n  # Suivi des boîtes ouvertes
-    opened[0] = True  # La première boîte est déverrouillée par défaut.
-    stack = [0]  # Commencez à explorer à partir de la boîte 0.
+    n = len(boxes)
+    opened = [False] * n
+    opened[0] = True
+    keys = [0]
 
-    while stack:
-        current_box = stack.pop()  # Prendre la boîte actuelle.
-        for key in boxes[current_box]:  # Explorer les clés dans cette boîte.
-            if key < n and not opened[key]:  # Si la cle ouvre une nouvelle boîte.
-                opened[key] = True  # marquer cette boîte comme ouverte.
-                stack.append(key)  # ajouter la boîte à la pile pour exploration.
+    while keys:
+        current_key = keys.pop()
+        for key in boxes[current_key]:
+            if key < n and not opened[key]:
+                opened[key] = True
+                keys.append(key)
 
-    return all(opened)  # Verifier Si toutes les boîtes sont ouvertes.
+    return all(opened)
